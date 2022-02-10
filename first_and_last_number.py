e94a89e19602c0bc0b54faa226c9f074b757793f
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-02-10 10:55:12 +0500

    Update first_and_last_number.py

--- first_and_last_number.py
+++ first_and_last_number.py
@@ -1,10 +1,9 @@
 numbers = [5,21,2,3,5,6,5]
 flag = False
 
-for index in range(len(numbers)):
-    if numbers[0]==5 and numbers[-1]==5: 
+if numbers[0]==numbers[-1]: 
         print("yes")
-    else:
-        print("not")
+ else:
+      print("not")
 
         

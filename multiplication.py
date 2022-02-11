8d9d31366935c28efbb0f566543290ad6a4eea4a
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-02-11 10:30:32 +0500

    Update multiplication.py

--- multiplication.py
+++ multiplication.py
@@ -1,4 +1,3 @@
-multiply=int(input("Enter a multiplicant:"))
-
 for number in range(10):
-    print(f"{multiply}X{number}={multiply*number}")
+    for num in range(10):
+        print(f"{number}X{num}={number*num}")
